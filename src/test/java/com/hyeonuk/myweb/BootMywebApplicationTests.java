00b034f8dbee1e0a6c9dba4e5423aed3893226c7
hyeonukk <abcam94@naver.com>
2023-02-14 18:21:46 +0900

    20230214

---
+++ src/test/java/com/hyeonuk/myweb/BootMywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeonuk.myweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMywebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
